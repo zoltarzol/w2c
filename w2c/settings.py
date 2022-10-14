bfaf5048d11b4431ec428870433d2b594fba520f
Cédric Duroisin <36937842+zoltarzol@users.noreply.github.com>
2022-10-14 22:22:07 +0200

    Update settings.py
    security commit

--- w2c/settings.py
+++ w2c/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kk+l151g-l4^-zvk*07kxh286h37*xb1qzw+(%=e-5bjnt)@hx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
